1d910dd570f8d9dc85ec059c7104562afeeeae38
gutzgutzgutz <gutzjung2549@gmail.com>
2025-10-29 22:48:20 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+var smth = prompt("Input Your name below")
+var greeting = "Hello"
+
+if (smth == "Weerapat") {
+    console.log(greeting)
+    alert("Hello King" + " " + smth)
+} else {
+    console.log(greeting)
+    alert("Hello" + " " + smth)
+}
